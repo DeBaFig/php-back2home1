1d95f338979728a8f2bc23abb78e5a9e204f8849
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-09-14 19:28:19 -0300

    layout basico

---
+++ config/variables.php
@@ -0,0 +1,11 @@
+<?php
+  // Variables
+  return [
+    "creatorName" => "Adriana e Denize",
+    "creatorUrl" => "https://github.com/DeBaFig/php-back2home1",
+    "support" => "mailto:back2home1.adm@gmail.com",
+    "documentation" => "https://github.com/DeBaFig/php-back2home1",
+    "facebookUrl" => "https://www.facebook.com/d.bassi91/",
+    "githubUrl" => "https://github.com/DeBaFig/php-back2home1",
+    "appDescription" => "A aplicação irá disponibilizar ao usuário acessar um catálogo com os imóveis à venda e realizar o cadastro de imóveis que deseja anunciar.",
+  ];
