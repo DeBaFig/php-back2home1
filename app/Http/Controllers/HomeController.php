263cff941126a5fa55eb14ad2569b86a18870379
Adriana Rocha <104075098+AdrianaEFRocha@users.noreply.github.com>
2022-09-19 17:58:53 -0300

    alteraçoes view about_business

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,16 +15,9 @@ class HomeController extends Controller
 
     public function about_business()
     {
-        $b = "<br>";
         $viewData = [];
         $viewData["title"] = "Sobre Back2Home1";
         $viewData["subtitle"] =  "Quem somos";
-        $viewData["description1"] =  "A sua CASA precisa ser o seu LAR. Para isso, a Back 2 Home 1 - Imóveis, nasceu no ano de 2022, para atuar no mercado da compra e venda de imóveis, dispondo na sua carteira os melhores imóveis à venda. 
-        Usamos a tecnologia e práticas inovadoras para criar soluções, visando a concretização de negócios que resultem em realização de sonhos.   
-        Oferece aos seus clientes assessoria especializada para financiamento, e uma capacitada equipe de corretores.
-        Por tudo isso, estamos aptos à prestar aos nossos clientes os melhores serviços, atendendo às suas expectativas e necessidades.";
-        $viewData["description2"] = "MISSÃO" . $b . "Transformar sonhos em realidade é o que nos motiva! Trabalhamos com pessoas que têm sonhos e objetivos, e, para nós, é um prazer saber que fazemos parte da transformação de histórias e conquistas.";
-        $viewData["description3"] = "VALORES" . $b . "CLIENTE – Satisfeito e valorizado. CONFIABILIDADE – Ética, Transparência e Integridade. RESPONSABILIDADE – Ambiental, Empresarial e Social. DESENVOLVIMENTO – Pessoal e Profissional.";
         return view('home.about_business')->with("viewData", $viewData);
     }    
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- Include Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}"> -->
 </head>
 
 <body>
