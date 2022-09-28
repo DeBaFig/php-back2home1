3cd1c0ab34caa1349382fbc6e65d9cba0b05937f
Adriana Rocha <104075098+AdrianaEFRocha@users.noreply.github.com>
2022-09-28 17:15:52 -0300

    formulario de cadastro de proprietários

--- resources/views/admin/add/add.blade.php
+++ resources/views/admin/add/add.blade.php
@@ -29,6 +29,8 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
   
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
@@ -79,7 +81,8 @@
     <!-- Main JS -->
     <script src="../assets/js/main.js"></script>
 
-    <!-- Page JS -->
+    <!-- Acoredeon -->
+    <script src="js/bootstrap.min.js"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
@@ -109,31 +112,98 @@
         </div>
         @endif
         <div class="card">
-        
-                  <div class="card mb-4">
-                    <h5 class="card-header">Cadastro de Imóveis</h5>
-            <!-- <div class="card-header text-center font-weight-bold"> -->
+            <div class="card mb-4">
+                <h5 class="card-header">Cadastro de Imóveis</h5>
             </div>
+            <div class="card mb-4">
+                <button type="button" class="card-header" data-bs-toggle="collapse" data-bs-target="#owner">
+                    Dados do Proprietário
+                </button>
+                <div class="collapse"id="owner">
+                    <form class="container flex-column w-100 d-flex justify-content-evenly" method="POST">
+                        <label for="exampleInputEmail1">Nome</label>
+                        <input type="text" id="desc" name="descrition" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">CPF</label>
+                        <input type="text" id="cpf" name="cpf"  class="form-control cpf-mask" placeholder="Ex.: 000.000.000-00" data-mask="000.000.000-00" maxlength="14" autocomplete="off" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" name="emailInput" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Endereço</label>
+                        <input type="text" id="adress" name="adress" class="form-control" placeholder="Rua" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Número</label>
+                        <input type="text" id="number" name="number" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Bairro</label>
+                        <input type="text" id="district" name="district" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Cidade</label>
+                        <input type="text" id="city" name="city" class="form-control"  required="true">
+                        <br>
+                        <label class="form-group">Estado</label>
+                        <select class="form-select " id="state">
+                            <option selected>Selecione</option>
+                            <option value="1">Acre</option>
+                            <option value="2">Alagoas</option>
+                            <option value="3">Amapá</option>
+                            <option value="4">Amazonas</option>
+                            <option value="5">Bahia</option>
+                            <option value="6">Ceará</option>
+                            <option value="7">Distrito Federal</option>
+                            <option value="8">Espírito Santo</option>
+                            <option value="9">Goiás</option>
+                            <option value="10">Maranhão</option>
+                            <option value="11">Mato Grosso</option>
+                            <option value="12">Mato Grosso do Sul</option>
+                            <option value="13">Minas Gerais</option>
+                            <option value="14">Pará</option>
+                            <option value="15">Paraíba</option>
+                            <option value="16">Paraná</option>
+                            <option value="17">Pernambuco</option>
+                            <option value="18">Piauí</option>
+                            <option value="19">Rio de Janeiro</option>
+                            <option value="20">Rio Grande do Norte</option>
+                            <option value="21">Rio Grande do Sul</option>
+                            <option value="22">Rondônia</option>
+                            <option value="23">Roraima</option>
+                            <option value="24">Santa Catarina</option>
+                            <option value="25">São Paulo</option>
+                            <option value="26">Sergipe</option>
+                            <option value="27">Tocantins</option>
+                        </select>
+                        <br>
+                        <label for="exampleInputEmail1">CEP</label>
+                        <input type="text" id="cep" name="cep" class="form-control" placeholder="Ex: 00000-000" data-mask="00000-000" maxlength="15" autocomplete="off" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Telefone</label>
+                        <input type="text" id="phone1" name="phone1" class="form-control" placeholder="Ex: (00) 00000-0000" data-mask="(00) 00000-0000" maxlength="15" autocomplete="off" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Celular</label>
+                        <input type="text" id="phone2" name="phone2" class="form-control" placeholder="Ex: (00) 00000-0000" data-mask="(00) 00000-0000" maxlength="15" required="true">
+                        <br>
+
+                        <input type="submit" form="frm" class="btn btn-block btn-outline-success float-right" style="width:20%" value="Salvar">
+                    </form>
+                </div>    
+            </div>
+        </div>
+            
+        <div class="card mb-4">
+            <button type="button" class="card-header" data-bs-toggle="collapse" data-bs-target="#immobile">
+                Dados do Imóvel
+            </button>
+            <div class="collapse"id="immobile">          
             <div class="card-body">
-                <form name="add" id="add" method="post" action="{{url('store-form')}}">
-                    @csrf
+                <form class="container flex-column w-30 d-flex justify-content-evenly" method="POST">
                     <div class="form-group">
                         <label class="mt-3">Referência: "Aqui colocar o ID automático"</label>
                         <!-- Ver aqui como fazer o campo Referência: se é colocado aleatório do se é gerado automaticamente. -->
                     </div>
-                    <div id="row">
-	                    <div id="">
-		                    <div class="col-md-9">
-			                    <div class="form-horizontal">
-				                    <div class="form-group">
-					                    <label class="col-md-3 control-label">Valor de Venda:</label>
-					                    <div class="col-md-9">
-						                    <input class="form-control" type="text" id="moeda" onkeyup="formataValor(this)" />
-					                    </div>
-				                </div>
-			                </div>
-		                </div>
-	                </div>
+                    <label for="exampleInputEmail1">Valor de Venda</label>
+                    <input type="text" id="prize" name="prize" class="form-control" placeholder="R$ 0,00" required="true">
+                    <br>   
                     <div class="col">
                         <label class="form-group">Objetivo:</label>
                         <select class="form-select" id="purpose">
@@ -162,10 +232,54 @@
                     <br>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Descrição:</label>
-                        <input type="text" id="title" name="title" class="form-control" required="true">
-
-                        <label for="exampleInputEmail1">Endereço:</label>
-                        <input type="text" id="title" name="title" class="form-control" required="true">
+                        <textarea name="description" id="description" class="form-control" required=""></textarea>
+                        <br>
+                        <label for="exampleInputEmail1">Endereço</label>
+                        <input type="text" id="adress_immobile" name="adress_immobile" class="form-control" placeholder="Rua" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Número</label>
+                        <input type="text" id="number_immobile" name="number_immobile" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Bairro</label>
+                        <input type="text" id="district_immobile" name="district_immobile" class="form-control" required="true">
+                        <br>
+                        <label for="exampleInputEmail1">Cidade</label>
+                        <input type="text" id="city_immobile" name="city_immobile" class="form-control"  required="true">
+                        <br>
+                        <label class="form-group">Estado</label>
+                        <select class="form-select " id="state_immobile">
+                            <option selected>Selecione</option>
+                            <option value="1">Acre</option>
+                            <option value="2">Alagoas</option>
+                            <option value="3">Amapá</option>
+                            <option value="4">Amazonas</option>
+                            <option value="5">Bahia</option>
+                            <option value="6">Ceará</option>
+                            <option value="7">Distrito Federal</option>
+                            <option value="8">Espírito Santo</option>                                    <option value="9">Goiás</option>
+                            <option value="10">Maranhão</option>
+                            <option value="11">Mato Grosso</option>
+                            <option value="12">Mato Grosso do Sul</option>
+                            <option value="13">Minas Gerais</option>
+                            <option value="14">Pará</option>
+                            <option value="15">Paraíba</option>
+                            <option value="16">Paraná</option>
+                            <option value="17">Pernambuco</option>
+                            <option value="18">Piauí</option>
+                            <option value="19">Rio de Janeiro</option>
+                            <option value="20">Rio Grande do Norte</option>
+                            <option value="21">Rio Grande do Sul</option>
+                            <option value="22">Rondônia</option>
+                            <option value="23">Roraima</option>
+                            <option value="24">Santa Catarina</option>
+                            <option value="25">São Paulo</option>
+                            <option value="26">Sergipe</option>
+                            <option value="27">Tocantins</option>
+                        </select>
+                        <br>
+                        <label for="exampleInputEmail1">CEP</label>
+                        <input type="text" id="cep_immobile" name="cep_immobile" class="form-control" placeholder="Ex: 00000-000" data-mask="00000-000" maxlength="15" autocomplete="off" required="true">
+                        <br>
                     </div>
                     <br>
                     <div class="col">
@@ -181,6 +295,7 @@
                             <option value="7">7</option>
                             <option value="8">8</option>
                         </select>
+                        <br>
                         <label class="form-group">Quantidade de Quartos:</label>
                         <select class="form-select " id="bedrooms">
                             <option selected>Selecione</option>
@@ -220,17 +335,20 @@
                         </select>
                     </div>
                     <br>
-                    <div>
-                    <label class="form-group">Área Útil:</label>
-                        <input type="text" name="nomeInput" required="true"> <label> m² </label> &ensp;  
-                        <label class="form-group"> Área Privativa: </label>
-                        <input type="text" name="nomeInput" required="true"> <label> m² </label> &ensp;
-                        <label class="form-group">Tamanho Total:</label>
-                        <input type="text" name="nomeInput" required="true"> <label> m² </label>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Área Útil:</label>
+                        <input type="text" id="useful" name="useful" class="form-control" placeholder="m²" data-mask="m²" maxlength="15" autocomplete="off" required="true">  
+                        <br>
+                        <label for="exampleInputEmail1"> Área Privativa: </label>
+                        <input type="text" id="private" name="private" class="form-control" placeholder="m²" data-mask="m²" maxlength="15" autocomplete="off" required="true">  
+                        <br>
+                        <label for="exampleInputEmail1">Tamanho Total:</label>
+                        <input type="text" id="full_size"name="full_size" class="form-control" placeholder="m²" data-mask="m²" maxlength="15" autocomplete="off" required="true">  
                     </div>
+                    <br>
                     <div>
                         <label for="exampleInputEmail1">Observações:</label>
-                        <textarea name="description" class="form-control" required=""></textarea>
+                        <textarea name="comments" class="form-control" required=""></textarea>
                     </div>
                     <br>
                     <div class="wrapper">
@@ -239,29 +357,27 @@
                             <input type="file" name="image[]" multiple="multiple"></label>
                             <input type="submit" form="frm" class="btn btn-block btn-outline-success float-right" style="width:20%" value="Enviar">
                         </form>
-                        <!-- Os colchetes no name do input indicam que os dados serão enviados em forma de array. É necessário para que o servidor receba corretamente os dados de todos os arquivos. -->
+                    <!-- Os colchetes no name do input indicam que os dados serão enviados em forma de array. É necessário para que o servidor receba corretamente os dados de todos os arquivos. -->
                     </div>
-                    <br><br>
+                </form>
+                <br><br>
 
-                    <!-- Upload Button -->
+                <!-- Upload Button -->
 
-                    <div class="row mt-2">
-                        <div class="col-md-4 offset-md-8 text-center mb-4">
-                            <button id="btnContinue" type="submit" form="frm" class="btn btn-block btn-outline-success float-right" data-toggle="tooltip" data-trigger="manual" data-placement="top" data-title="Continue">
+                <div class="row mt-2">
+                    <div class="col-md-4 offset-md-8 text-center mb-4">
+                        <button id="btnContinue" type="submit" form="frm" class="btn btn-block btn-outline-success float-right" data-toggle="tooltip" data-trigger="manual" data-placement="top" data-title="Continue">
                             Continue<span id="btnContinueIcon" class="fa fa-chevron-circle-right ml-2"></span><span id="btnContinueLoading" class="fa fa-spin fa-spinner ml-2" style="display:none"></span>
-                            </button>
-                        </div>
+                        </button>
                     </div>
-                    <br>
-
-                   
-                </div>
+                </div>                
             </div>
         </div>
     </div>
 </body>
 
 </html>
+
 
 <!-- ver instruções upload de arquivo em https://www.jqueryscript.net/form/multi-image-uploader-bootstrap.html -->
 @endsection
